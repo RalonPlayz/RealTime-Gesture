b43b4179a33ea665f0c076f6ecfcb3442dc1c178
RalonPlayz <79267114+RalonPlayz@users.noreply.github.com>
2021-06-15 15:32:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function setup() {
+    canvas = createCanvas(550, 550);
+    canvas.position(260, 150);
+    video = createCapture(VIDEO, modelLoaded);
+    video.size(550, 500, );
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log("poseNet is initialized");
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+    }
+}
+
+function draw() {
+    background('#969897');
+
+}
